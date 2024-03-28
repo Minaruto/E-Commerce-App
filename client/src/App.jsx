2acf67d42b644c7cb670fa0a5bce6c82ddbe9f18
MyroneGonsalves <myrone.gonsalves@outlook.com>
2024-03-28 19:53:11 +0400

    Created login & register pages | Added routes to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import {Route, Routes} from 'react-router-dom'
 import IndexPage from './pages/IndexPage'
 import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
 import Layout from './Components/Layout'
 
 export default function App() {
@@ -11,6 +12,7 @@ export default function App() {
       <Route path='/' element={<Layout />}>
         <Route index element={<IndexPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Route>
     </Routes>
   )
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,16 +1,17 @@
 import { Form, Link } from "react-router-dom";
 
-export default function LoginPage(){
+export default function RegisterPage(){
     return(
         <div className="mt-4 grow flex items-center justify-around">
             <div className="mb-64">
-                <h1 className="text-4xl text-center mb-10">Login to your account</h1>
+                <h1 className="text-4xl text-center mb-10">Create an account</h1>
                 <form className="max-w-md mx-auto">
+                    <input type="text" placeholder="Enter your name"/>
                     <input type="email" placeholder="Enter your email"/>
                     <input type="password" placeholder="Enter your password"/>
                     <button className="primary">Login</button>
                     <div className="py-2 text-center text-gray-600">
-                        Don't have an account yet? <Link to={'/register'} className="underline text-black"> Register now!</Link>
+                        Already a member? <Link to={'/login'} className="underline text-black"> Login!</Link>
                     </div>
                 </form>
             </div>
